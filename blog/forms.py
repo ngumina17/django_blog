8d38a7c985bc878e66c27377da5534ce05cfa786
Jamrod <myfavoritenoisemaker@gmail.com>
2020-02-12 10:31:49 -0700

    added author auto-populating new blog post and un-editable

--- blog/forms.py
+++ blog/forms.py
@@ -1,15 +1,19 @@
 from django import forms
 from .models import Author, Blog
 
+
 class AuthorForm(forms.ModelForm):
     class Meta:
         model = Author
         fields = ('name', 'location', 'photo_url',)
 
+
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ('title', 'author', 'content', 'photo_url',)
+        widgets = {'author': forms.HiddenInput()}
+
 
 class SearchBar(forms.Form):
-        fields = ('search')
+    fields = ('search')
